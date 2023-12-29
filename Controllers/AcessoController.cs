b04197494d8e3e7141c83474c37c30ec5bd7cf43
Renatatr <78363362+Renatatr@users.noreply.github.com>
2023-12-29 14:17:19 -0300

    Controle para acesso permitido com relação a idade

---
+++ Controllers/AcessoController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsuarioAPI.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class AcessoController : ControllerBase
+{
+    [HttpGet]
+    [Authorize(Policy = "IdadeMinima")]
+    public IActionResult Get()
+    {
+        return Ok("Acesso permitido!");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using UsuarioAPI.Authorization;
 using UsuarioAPI.Data;
 using UsuarioAPI.Models;
 using UsuarioAPI.Services;
@@ -23,11 +28,34 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization> ();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+        JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("324J3R09EOIRJWE09R23T5T4RHG45H")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("IdadeMinima", policy =>
+        policy.AddRequirements(new IdadeMinima(18))
+    );
+});
 
 var app = builder.Build();
 
@@ -39,7 +67,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
